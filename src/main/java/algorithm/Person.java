c83661c3be0a175b7fea5608882a42fa4eb5d334
oscarbelm <48737004+oscarbelm@users.noreply.github.com>
2022-10-06 15:32:48 -0500

    Checkpoint

--- src/main/java/algorithm/Person.java
+++ src/main/java/algorithm/Person.java
@@ -3,28 +3,17 @@ package algorithm;
 import java.util.Date;
 
 public class Person {
-	public String name;
-	public Date birthDate;
+	private String name;
+	private Date birthDate;
 
-	public static Person create(String name, Date birthDate){
-		Person person = new Person();
-		person.name = name;
-		person.birthDate = birthDate;
-
-		return person;
-	}
-
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
+	public Person(String name, Date birthDate){
 		this.name = name;
-	}
-	public Date getBirthDate() {
-		return birthDate;
-	}
-	public void setBirthDate(Date birthDate) {
 		this.birthDate = birthDate;
 	}
+	
+	public Date birthDate() {
+		return birthDate;
+	}
+	
 }
 
